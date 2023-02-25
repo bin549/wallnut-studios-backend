89a21397970ff707467fe9d367c8c17a0c394d3c
bin549 <549288160@qq.com>
2023-02-25 16:21:28 +0800

    practice #1

---
+++ api/config/config.py
@@ -0,0 +1,25 @@
+import os
+from decouple import config
+
+
+
+
+class Config:
+    SECRET_KEY=config('SECRET_KEY','Secret')
+
+
+class DevConfig(Config):
+    pass
+
+class TestConfig(Config):
+	pass
+
+class ProdConfig(Config):
+    pass
+	
+ 
+config_dict={
+    'dev':DevConfig,
+    'testing':TestConfig,
+    'production':ProdConfig
+}
