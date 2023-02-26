d1147fb26980dfcf687af536eb3e56420db356e3
bin549 <549288160@qq.com>
2023-02-26 15:33:26 +0800

    JWT

--- api/config/config.py
+++ api/config/config.py
@@ -1,24 +1,44 @@
 import os
 from decouple import config
+from datetime import timedelta
+import re
 
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+uri = config("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 
 class Config:
-    SECRET_KEY=config('SECRET_KEY','Secret')
+    SECRET_KEY = config('SECRET_KEY', 'Secret')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(minutes=30)
+    JWT_SECRET_KEY = config('JWT_SECRET_KEY')
 
 
 class DevConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'db.sqlite3')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    DEBUG = True
+
 
 class TestConfig(Config):
-	pass
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+
 
 class ProdConfig(Config):
-    pass
-	
- 
-config_dict={
-    'dev':DevConfig,
-    'testing':TestConfig,
-    'production':ProdConfig
+    SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = config('DEBUG', cast=bool)
+
+
+config_dict = {
+    'dev': DevConfig,
+    'testing': TestConfig,
+    'production': ProdConfig
 }
--- api/orders/views.py
+++ api/orders/views.py
@@ -1,10 +1,36 @@
 from flask_restx import Namespace, Resource
 
-order_namespace=Namespace('orders',description="Namespace for orders")
+order_namespace = Namespace('orders', description="Namespace for orders")
 
 
+@order_namespace.route('/orders/')
+class OrderGetCreate(Resource):
+    def get(self):
+        pass
 
-@order_namespace.route('/')
-class HelloOrder(Resource):
-	def get(self):
-		return {'message', 'Hello Order'}
+    def post(self):
+        pass
+
+
+@order_namespace.route('/order/<int:order_id>')
+class GetUpdateDelete(Resource):
+    def get(self):
+        pass
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
+
+
+@order_namespace.route('/user/<int:user_id>/order/<int:order_id>/')
+class GetSpecificOrderByUser(Resource):
+    def get(self):
+        pass
+
+
+@order_namespace.route('/order/status/<int:order_id>')
+class UpdateOrderStatus(Resource):
+    def patch(self, order_id):
+        pass
