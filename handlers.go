f62ddfc2529752fc27d64fe6029d4d290328a4ce
--# <--#@gmail.com>
2024-02-05 13:44:38 +0800

    chore: enable CORS.

--- handlers.go
+++ handlers.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	return
+	var payload = struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status:  "active",
+		Message: "Go Go",
+		Version: "1.0.0",
+	}
+	fmt.Println(payload)
 }
--- routes.go
+++ routes.go
@@ -9,6 +9,7 @@ import (
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
+	mux.Use(app.enableCORS)
 	mux.Get("/", app.Home)
 
 	return mux
