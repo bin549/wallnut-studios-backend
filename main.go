f5b9d396063c90785533be0b8b93b531dbb48232
--# <--#@gmail.com>
2024-02-05 22:48:56 +0800

    chore: improve skill handler #2.

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ type application struct {
 
 func main() {
 	var app application
-	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=__2018bb dbname=wallnut-studios sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.StringVar(&app.DSN, "dsn", "host=124.71.65.161 port=5432 user=postgres password=__2018bb dbname=wallnut-studios sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
 	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
 	flag.Parse()
 	conn, err := app.connectToDB()
--- middleware.go
+++ middleware.go
@@ -1,17 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		origin := r.Header.Get("Origin")
+		allowedOrigins := []string{"http://localhost:3000", "http://124.71.65.161:3000"}
+		allowed := false
+		for _, allowOrigin := range allowedOrigins {
+			if origin == allowOrigin {
+				allowed = true
+				break
+			}
+		}
+		if allowed {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		if r.Method == "OPTIONS" {
 			w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, X-CSRF-Token, Authorization")
 			return
-		} else {
-			h.ServeHTTP(w, r)
 		}
+		h.ServeHTTP(w, r)
 	})
 }
